4d33b25fa326de92d2c6d415ccdb6b4667823ec7
unknown <mdanwarhossain1996bd@gmail.com>
2021-10-13 02:07:54 -0600

    Project Running

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,17 +6,17 @@ const Navbar = () => {
             <nav className="nav" id="nav">
                 <div className="navbar">
                     <ul className="left">
-                        <li><a href="">BD Express</a></li>
+                        <li><a href="" className="fat">BD Express</a></li>
                     </ul>
                     <ul className="right">
-                        <li>
-                            <a href="">
-                               <div className="shop">
-                                   <span className="shoppingCart"><i className="fas fa-cart-plus"></i></span>
-                                   <span>0</span>
-                               </div>
-                            </a>
+
+
+                        <li className="nav-item">
+                            <a href="" className="nav-link">
+                                <span className="shoppingCart"><i className="fas fa-cart-plus"></i> </span> <span className="count">0</span> </a>
                         </li>
+
+
                     </ul>
                 </div>
             </nav>
