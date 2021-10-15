fd37eccd32e82d3eff6f557caba0fe0d0cd5d2a4
unknown <mdanwarhossain1996bd@gmail.com>
2021-10-15 10:26:22 -0600

    Running

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -14,6 +14,11 @@ const Cart = () => {
                             <span className="cart-image"><img src={cart.image} alt="Not Found" /> </span>
                             <span className="cart-product-name">{cart.name}</span>
                             <span className="cart-product-price">{cart.price}</span>
+                            <span className="inc"><i className="fas fa-plus"></i></span>
+                            <span className="product-quantity">{cart.qty}</span>
+                            <span className="dec"><i className="fas fa-minus"></i></span>
+                            <span className="product-total-price">500.00</span>
+                            <span className="delete-product"><i className="fas fa-trash-alt"></i></span>
                         </div>
                     ))
                     : <div>
